2ded4f0928ab7abc3e802de73ddae72b1c93ee23
Melfric <wegweg3eg@abv.bg>
2023-01-06 09:34:28 +0200

    started adding planets

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Sun from "./components/sun";
 import { Stars } from "@react-three/drei";
 import { OrbitControls } from '@react-three/drei'
 import * as THREE from 'three';
+import Mars from "./components/Planet";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
             <OrbitControls maxDistance={55} />
             <Stars radius={240} speed={0} saturation={1} />
             <Sun name="sun" />
+            <Mars name="mars" position={[5, 1, 1]}/>
         </Canvas>
 
   );
--- src/components/sun.tsx
+++ src/components/sun.tsx
@@ -2,28 +2,27 @@ import React from 'react';
 import { useRef, useEffect } from 'react';
 import { useFrame } from "@react-three/fiber";
 import {EffectComposer, Outline, SelectiveBloom} from "@react-three/postprocessing";
+import {Object3D} from "three";
 
 function Sun(props: any) {
-    const ref = useRef();
+    const ref = useRef<Object3D>(null!);
     useEffect(() => {
     console.log(ref);
     });
 
     useFrame((state, delta) => {
-        // @ts-ignore
         ref.current.rotation.y += 0.15 * delta;
-        // @ts-ignore
         ref.current.rotation.x += 0.15 * delta;
-        // @ts-ignore
         //ref.current.position.y += Math.sin(state.clock.getElapsedTime()) / 200;
     });
   return (
     <>
-        <EffectComposer>
+        <EffectComposer >
             <SelectiveBloom selection={ref}  luminanceThreshold={0.6} intensity={1.8} />
+            <Outline  visibleEdgeColor={0xf48037} edgeStrength={5}  />
         </EffectComposer>
         <mesh {...props} ref = {ref} >
-            <sphereGeometry attach="geometry" args={[0.4,32,32]} />
+            <sphereGeometry attach="geometry" args={[0.5,32,32]} />
             <meshBasicMaterial color={"yellow"} />
         </mesh>
 
